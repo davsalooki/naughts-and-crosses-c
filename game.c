47509c152f9ce2a7c775caa69ba7d05c31a73ca7
davsalooki <46581656+davsalooki@users.noreply.github.com>
2023-09-03 18:11:46 +1000

    first stage

---
+++ game.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "game.h"
+
+int game() {
+    printf("This is game");
+}
--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("testing\n");
+    printf("Welcome to naughts and crosses!\n");
+    printf("This is my first mini project in C.\n");
+    printf("I will be learning C++ after a few C projects.\n\n");
+    printf("Press any key to continue.\n");
+    char ch = getchar();
+    if (ch != 'q') {
+        while (getchar() != '\n');
+        main();
+    }
+    return 0;
 }
+
